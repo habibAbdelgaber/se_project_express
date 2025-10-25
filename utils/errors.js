8574ca745d9f2cc9939b5975cd673b6cb6dd19a2
Habib Abdelgaber <habib.abdelgaber@yahoo.com>
2025-10-25 17:01:05 +0300

    chore: fix ESLint import order, unused param, and console usage

--- utils/errors.js
+++ utils/errors.js
@@ -55,7 +55,7 @@ function handleMongooseError(originalErr) {
 }
 
 // Express error handling middleware
-function errorHandler(err, req, res) {
+function errorHandler(err, req, res, _next) {
   // Normalize Mongoose/Mongo errors without reassigning the parameter
   const normalized = (err && (err.name === 'ValidationError' || err.name === 'CastError' || err.code === 11000))
     ? handleMongooseError(err)
